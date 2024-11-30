3f4ad6a75675055ad32a51e11358e17998d95ee4
JDog725 <tjmoran@nic.edu>
2024-11-30 01:58:15 +0000

    Problem 2 done

--- 2.cpp
+++ 2.cpp
@@ -6,12 +6,14 @@ const int SIZE = 11;
 //function prototypes
 void displayArray(int array[], int size);
 //add prototype for reverseArray
+void reverseArray(int array[], int size);
 
 int main()
 {
     int numbers[SIZE] ={7,-2,4,6, 9, 11,-5, 12, 0, 2, 3};
     displayArray(numbers, SIZE);
     //call reverseArray here
+    reverseArray(numbers, SIZE);
 
     displayArray(numbers, SIZE);
     return 0;
@@ -22,7 +24,15 @@ int main()
 *This function reverses the array in place. (e.g. array has values {3, 5, 12, 9, 2}
 *will be reversed as {2, 9, 12, 5, 3})
 *********************************************************************************/
-
+void reverseArray(int array[], int size)
+{
+    for(int i = 0; i < (size/2); i++)
+    {
+        int temp = array[i];
+        array[i] = array[(size - i)-1];
+        array[(size - i)-1] = temp;
+    }
+}
 
 
 
